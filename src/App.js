fff0b6cc155bfbac988abdc8e84710fa8c869708
Mohitpanwar09 <mohitpanwar9420@gmail.com>
2024-06-15 00:03:52 +0530

    Added routing

--- src/App.js
+++ src/App.js
@@ -1,45 +1,25 @@
 import './App.css';
+import Post from './components/Post';
+import Header from './components/Header';
+import {Route,Routes} from "react-router-dom";
+import Login from './components/Login';
+import Register from './components/Register';
 
 function App() {
   return (
-    <main>
-      <header>
-        <a href = "" className ="logo" >My Blog</a>
-        <nav>
-          <a href="">Login</a>
-          <a href="">Register</a>
-        </nav>
-      </header>
-      <div className="post">
-        <div className="image">
-          <img src="https://cupofjo.com/wp-content/uploads/2024/06/20240531_COJ_Article_020-scaled-fotor-20240611161335-768x523.jpg"/>
-        </div>
-        <div className="text">
-          <h2>Title of the blog post</h2>
-          <p className="info">
-            <a className="author">Mohit</a>
-            <time>2024-06-12</time>
-          </p>
-          <p className="summary">descreption of the blog post  descreption of the blog post   descreption of the blog post   descreption of the blog post   descreption of the blog post   descreption of the blog post   descreption of the blog post   descreption of the blog post   
-          </p>
-        </div>
-      </div>  
+      <Routes>
+        <Route index element={
+          <main>
+            <Header/>
+            <Post/>
+          </main>
+        }/>
+        
+        <Route path={'/login'} element={<Login/>}/>
+        <Route path={'/register'} element={<Register/>}/>
 
-      <div className="post">
-        <div className="image">
-          <img src="https://cupofjo.com/wp-content/uploads/2024/06/20240531_COJ_Article_020-scaled-fotor-20240611161335-768x523.jpg"/>
-        </div>
-        <div className="text">
-          <h2>Title of the blog post</h2>
-          <p className="info">
-            <a className="author">Mohit</a>
-            <time>2024-06-12</time>
-          </p>
-          <p className="summary">descreption of the blog post  descreption of the blog post   descreption of the blog post   descreption of the blog post   descreption of the blog post   descreption of the blog post   descreption of the blog post   descreption of the blog post   
-          </p>
-        </div>
-      </div>        
-    </main>
+      </Routes>
+    
   );
 }
 
---
+++ src/components/Register.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function Register() {
+  return (
+    <div>welcome to Register</div>
+  )
+}
+
+export default Register
