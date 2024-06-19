76c943e41b2ad68e9bf7152ce0f2d4c167627fd6
Mohitpanwar09 <mohitpanwar9420@gmail.com>
2024-06-19 23:45:24 +0530

    connect backend for register

--- src/components/Register.jsx
+++ src/components/Register.jsx
@@ -1,12 +1,49 @@
 import React from 'react'
+import { useState } from 'react'
+import axios from 'axios'
 
 function Register() {
+
+  const [user, setUser] = useState({
+    username : "",
+    email : "",
+    password : ""
+  });
+
+  const handleSubmit = async(e)=>{
+    e.preventDefault();
+    try {
+        const response = await axios.post('http://localhost:5000/user/register',user);
+
+        if(response.status === 200){
+          console.log(response);
+        }
+        
+    } catch (error) {
+      console.log(error);
+    }
+  }
+  
+  const handleChange = (e)=>{
+    let name = e.target.name;
+    let value = e.target.value;
+
+    setUser({
+      ...user,
+      [name] : value
+    })
+
+    console.log(user);
+  }
+
   return (
-    <form action='' className='register'>
+    <form onSubmit={handleSubmit} className='register' >
       <h2>Register</h2>
-      <input type='text' placeholder='Username'/>
-      <input type='password' placeholder='Password'/>
-      <button>Register</button>
+      <input type='text' placeholder='Username' name='username' value ={user.username} onChange={handleChange}/>
+      <input type='password' placeholder='Password' name='password' value ={user.password} onChange={handleChange}/>
+      <input type='text' placeholder='Email' name='email' value = {user.email} onChange={handleChange}/>
+
+      <button type='submit'>Register</button>
     </form>
   )
 }
